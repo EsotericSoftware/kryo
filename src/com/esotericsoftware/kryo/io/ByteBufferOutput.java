7d840a0a5f94a8c8aca0ea1c0796ab5ff48381da
Sietse T. Au <sietse.au@gmail.com>
2016-02-23 12:42:59 +0100

    Fix #389 revert ByteOrder on overflow in require during writeVar*() during writeVar*() a KryoException caused by require(int) did not revert the byteOrder

--- src/com/esotericsoftware/kryo/io/ByteBufferOutput.java
+++ src/com/esotericsoftware/kryo/io/ByteBufferOutput.java
@@ -200,12 +200,16 @@ public class ByteBufferOutput extends Output {
 	/** @return true if the buffer has been resized. */
 	protected boolean require (int required) throws KryoException {
 		if (capacity - position >= required) return false;
-		if (required > maxCapacity)
+		if (required > maxCapacity) {
+			niobuffer.order(byteOrder);
 			throw new KryoException("Buffer overflow. Max capacity: " + maxCapacity + ", required: " + required);
+		}
 		flush();
 		while (capacity - position < required) {
-			if (capacity == maxCapacity)
+			if (capacity == maxCapacity) {
+				niobuffer.order(byteOrder);
 				throw new KryoException("Buffer overflow. Available: " + (capacity - position) + ", required: " + required);
+			}
 			// Grow buffer.
 			if (capacity == 0) capacity = 1;
 			capacity = Math.min(capacity * 2, maxCapacity);
