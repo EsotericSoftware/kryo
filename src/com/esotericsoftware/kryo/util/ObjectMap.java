2b8e6fddddd7a468f240ea59f90ef1dc2566a0cf
Mulugeta Mammo <muler.mammo@gmail.com>
2016-03-01 21:05:56 -0800

    Modified utility hash maps to gracefully handle very large number of entries.

--- src/com/esotericsoftware/kryo/util/ObjectMap.java
+++ src/com/esotericsoftware/kryo/util/ObjectMap.java
@@ -24,17 +24,19 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Random;
 
-/** An unordered map. This implementation is a cuckoo hash map using 3 hashes, random walking, and a small stash for problematic
- * keys. Null keys are not allowed. Null values are allowed. No allocation is done except when growing the table size. <br>
+/** An unordered map. This implementation is a cuckoo hash map using 3 hashes (if table size is less than 2^16) or 4 hashes
+ *  (if table size is greater than or equal to 2^16), random walking, and a small stash for problematic keys Null keys are not
+ *  allowed. Null values are allowed. No allocation is done except when growing the table size. <br>
  * <br>
  * This map performs very fast get, containsKey, and remove (typically O(1), worst case O(log(n))). Put may be a bit slower,
  * depending on hash collisions. Load factors greater than 0.91 greatly increase the chances the map will have to rehash to the
  * next higher POT size.
  * @author Nathan Sweet */
 public class ObjectMap<K, V> {
-	private static final int PRIME1 = 0xbe1f14b1;
-	private static final int PRIME2 = 0xb4b82e39;
-	private static final int PRIME3 = 0xced1c241;
+	// primes for hash functions 2, 3, and 4
+	private static final int PRIME2 = 0xbe1f14b1;
+	private static final int PRIME3 = 0xb4b82e39;
+	private static final int PRIME4 = 0xced1c241;
 
 	static Random random = new Random();
 
@@ -48,6 +50,7 @@ public class ObjectMap<K, V> {
 	private int hashShift, mask, threshold;
 	private int stashCapacity;
 	private int pushIterations;
+	private boolean isBigTable;
 
 	/** Creates a new map with an initial capacity of 32 and a load factor of 0.8. This map will hold 25 items before growing the
 	 * backing table. */
@@ -70,6 +73,9 @@ public class ObjectMap<K, V> {
 
 		if (loadFactor <= 0) throw new IllegalArgumentException("loadFactor must be > 0: " + loadFactor);
 		this.loadFactor = loadFactor;
+
+		// big table is when capacity >= 2^16
+		isBigTable = (capacity >>> 16) != 0 ? true : false;
 
 		threshold = (int)(capacity * loadFactor);
 		mask = capacity - 1;
@@ -97,7 +103,10 @@ public class ObjectMap<K, V> {
 	}
 
 	private V put_internal (K key, V value) {
+		// avoid getfield opcode
 		K[] keyTable = this.keyTable;
+		int mask = this.mask;
+		boolean isBigTable = this.isBigTable;
 
 		// Check for existing keys.
 		int hashCode = key.hashCode();
@@ -123,6 +132,18 @@ public class ObjectMap<K, V> {
 			V oldValue = valueTable[index3];
 			valueTable[index3] = value;
 			return oldValue;
+		}
+
+		int index4 = -1;
+		K key4 = null;
+		if (isBigTable) {
+			index4 = hash4(hashCode);
+			key4 = keyTable[index4];
+			if (key.equals(key4)) {
+				V oldValue = valueTable[index4];
+				valueTable[index4] = value;
+				return oldValue;
+			}
 		}
 
 		// Update key in the stash.
@@ -156,7 +177,14 @@ public class ObjectMap<K, V> {
 			return null;
 		}
 
-		push(key, value, index1, key1, index2, key2, index3, key3);
+		if (isBigTable && key4 == null) {
+			keyTable[index4] = key;
+			valueTable[index4] = value;
+			if (size++ >= threshold) resize(capacity << 1);
+			return null;
+		}
+
+		push(key, value, index1, key1, index2, key2, index3, key3, index4, key4);
 		return null;
 	}
 
@@ -197,21 +225,37 @@ public class ObjectMap<K, V> {
 			return;
 		}
 
-		push(key, value, index1, key1, index2, key2, index3, key3);
+		int index4 = -1;
+		K key4 = null;
+		if (isBigTable) {
+			index4 = hash4(hashCode);
+			key4 = keyTable[index4];
+			if (key4 == null) {
+				keyTable[index4] = key;
+				valueTable[index4] = value;
+				if (size++ >= threshold) resize(capacity << 1);
+				return;
+			}
+		}
+
+		push(key, value, index1, key1, index2, key2, index3, key3, index4, key4);
 	}
 
-	private void push (K insertKey, V insertValue, int index1, K key1, int index2, K key2, int index3, K key3) {
+	private void push (K insertKey, V insertValue, int index1, K key1, int index2, K key2, int index3, K key3, int index4, K key4) {
+		// avoid getfield opcode
 		K[] keyTable = this.keyTable;
 		V[] valueTable = this.valueTable;
 		int mask = this.mask;
+		boolean isBigTable = this.isBigTable;
 
 		// Push keys until an empty bucket is found.
 		K evictedKey;
 		V evictedValue;
 		int i = 0, pushIterations = this.pushIterations;
+		int n = isBigTable ? 4 : 3;
 		do {
 			// Replace the key and value for one of the hashes.
-			switch (random.nextInt(3)) {
+			switch (random.nextInt(n)) {
 			case 0:
 				evictedKey = key1;
 				evictedValue = valueTable[index1];
@@ -224,11 +268,17 @@ public class ObjectMap<K, V> {
 				keyTable[index2] = insertKey;
 				valueTable[index2] = insertValue;
 				break;
-			default:
+			case 2:
 				evictedKey = key3;
 				evictedValue = valueTable[index3];
 				keyTable[index3] = insertKey;
 				valueTable[index3] = insertValue;
+				break;
+			default:
+				evictedKey = key4;
+				evictedValue = valueTable[index4];
+				keyTable[index4] = insertKey;
+				valueTable[index4] = insertValue;
 				break;
 			}
 
@@ -259,6 +309,17 @@ public class ObjectMap<K, V> {
 				valueTable[index3] = evictedValue;
 				if (size++ >= threshold) resize(capacity << 1);
 				return;
+			}
+
+			if (isBigTable) {
+				index4 = hash4(hashCode);
+				key4 = keyTable[index4];
+				if (key4 == null) {
+					keyTable[index4] = evictedKey;
+					valueTable[index4] = evictedValue;
+					if (size++ >= threshold) resize(capacity << 1);
+					return;
+				}
 			}
 
 			if (++i == pushIterations) break;
@@ -292,7 +353,15 @@ public class ObjectMap<K, V> {
 			index = hash2(hashCode);
 			if (!key.equals(keyTable[index])) {
 				index = hash3(hashCode);
-				if (!key.equals(keyTable[index])) return getStash(key);
+				if (!key.equals(keyTable[index])) {
+					if (isBigTable) {
+						index = hash4(hashCode);
+						if (!key.equals(keyTable[index])) return getStash(key);
+					}
+					else {
+						return getStash(key);
+					}
+				}
 			}
 		}
 		return valueTable[index];
@@ -313,7 +382,15 @@ public class ObjectMap<K, V> {
 			index = hash2(hashCode);
 			if (!key.equals(keyTable[index])) {
 				index = hash3(hashCode);
-				if (!key.equals(keyTable[index])) return getStash(key, defaultValue);
+				if (!key.equals(keyTable[index])) {
+					if (isBigTable) {
+						index = hash4(hashCode);
+						if (!key.equals(keyTable[index])) return getStash(key, defaultValue);
+					}
+					else {
+						return getStash(key, defaultValue);
+					}
+				}
 			}
 		}
 		return valueTable[index];
@@ -353,6 +430,17 @@ public class ObjectMap<K, V> {
 			valueTable[index] = null;
 			size--;
 			return oldValue;
+		}
+
+		if (isBigTable) {
+			index = hash4(hashCode);
+			if (key.equals(keyTable[index])) {
+				keyTable[index] = null;
+				V oldValue = valueTable[index];
+				valueTable[index] = null;
+				size--;
+				return oldValue;
+			}
 		}
 
 		return removeStash(key);
@@ -441,7 +529,14 @@ public class ObjectMap<K, V> {
 			index = hash2(hashCode);
 			if (!key.equals(keyTable[index])) {
 				index = hash3(hashCode);
-				if (!key.equals(keyTable[index])) return containsKeyStash(key);
+				if (!key.equals(keyTable[index])) {
+					if (isBigTable) {
+						index = hash4(hashCode);
+						if (!key.equals(keyTable[index])) return containsKeyStash(key);
+					} else {
+						return containsKeyStash(key);
+					}
+				}
 			}
 		}
 		return true;
@@ -491,6 +586,9 @@ public class ObjectMap<K, V> {
 		stashCapacity = Math.max(3, (int)Math.ceil(Math.log(newSize)) * 2);
 		pushIterations = Math.max(Math.min(newSize, 8), (int)Math.sqrt(newSize) / 8);
 
+		// big table is when capacity >= 2^16
+		isBigTable = (capacity >>> 16) != 0 ? true : false;
+
 		K[] oldKeyTable = keyTable;
 		V[] oldValueTable = valueTable;
 
@@ -515,6 +613,11 @@ public class ObjectMap<K, V> {
 
 	private int hash3 (int h) {
 		h *= PRIME3;
+		return (h ^ h >>> hashShift) & mask;
+	}
+
+	private int hash4 (int h) {
+		h *= PRIME4;
 		return (h ^ h >>> hashShift) & mask;
 	}
 
