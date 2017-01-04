968c2404e9f8e64ffb2bc1b5eb24219e92f1bf81
andilem <andreaslmmr@gmail.com>
2017-01-04 13:24:54 +0100

    Fix IdentityMap constructor
    Fix read of uninitialized variable in constructor of IdentityMap

--- src/com/esotericsoftware/kryo/util/IdentityMap.java
+++ src/com/esotericsoftware/kryo/util/IdentityMap.java
@@ -67,7 +67,7 @@ public class IdentityMap<K, V> {
 	 * items before growing the backing table. */
 	public IdentityMap (int initialCapacity, float loadFactor) {
 		if (initialCapacity < 0) throw new IllegalArgumentException("initialCapacity must be >= 0: " + initialCapacity);
-		if (capacity > 1 << 30) throw new IllegalArgumentException("initialCapacity is too large: " + initialCapacity);
+		if (initialCapacity > 1 << 30) throw new IllegalArgumentException("initialCapacity is too large: " + initialCapacity);
 		capacity = ObjectMap.nextPowerOfTwo(initialCapacity);
 
 		if (loadFactor <= 0) throw new IllegalArgumentException("loadFactor must be > 0: " + loadFactor);
