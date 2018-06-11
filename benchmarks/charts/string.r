718bde49aec906f116991a626fc88d5e23f614f1
NathanSweet <nathan.sweet@gmail.com>
2018-06-11 05:31:46 +0200

    Better y axis labels.

--- benchmarks/charts/string.r
+++ benchmarks/charts/string.r
@@ -5,7 +5,7 @@ data = jmhCSV("string.csv")
 
 data = data[,grep("^(Benchmark|Score|Error|bufferType)$", colnames(data))] # keep only these columns
 
-g = jmhBarChart(data, "bufferType", "Buffer type", "Operation", "Operations per second", "StringBenchmark")
+g = jmhBarChart(data, "bufferType", "Buffer type", "Operation", "Seconds per 12M operations", "StringBenchmark")
 
 if (!rstudio) png("string.png", 1024, 445)
 grid.arrange(g)
--- benchmarks/charts/variableEncoding.r
+++ benchmarks/charts/variableEncoding.r
@@ -5,7 +5,7 @@ data = jmhCSV("variableEncoding.csv")
 
 data = data[,grep("^(Benchmark|Score|Error|bufferType)$", colnames(data))] # keep only these columns
 
-g = jmhBarChart(data, "bufferType", "Buffer type", "Operation", "Operations per second", "VariableEncodingBenchmark")
+g = jmhBarChart(data, "bufferType", "Buffer type", "Operation", "Seconds per 150M operations", "VariableEncodingBenchmark")
 
 if (!rstudio) png("variableEncoding.png", 1024, 445)
 grid.arrange(g)
