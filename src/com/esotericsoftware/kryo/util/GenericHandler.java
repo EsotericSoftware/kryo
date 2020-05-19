ff70845cec6ef36da954b6e4b942e62d2a6ef850
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2020-05-19 10:07:36 +0200

    JavaDoc

--- src/com/esotericsoftware/kryo/util/GenericHandler.java
+++ src/com/esotericsoftware/kryo/util/GenericHandler.java
@@ -24,7 +24,11 @@ import java.lang.reflect.TypeVariable;
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+/** Provides an interface on how generics are handled. */
 public interface GenericHandler {
+	/** Sets the type that is currently being serialized. Must be balanced by {@link #popGenericType()}. Between those calls, the
+	 * {@link GenericType#getTypeParameters() type parameters} are returned by {@link #nextGenericTypes()} and	
+	 * {@link #nextGenericClass()}. */
 	public void pushGenericType (GenericType fieldType);
 	
 	/** Removes the generic types being tracked since the corresponding {@link #pushGenericType(GenericType)}. This is safe to call
