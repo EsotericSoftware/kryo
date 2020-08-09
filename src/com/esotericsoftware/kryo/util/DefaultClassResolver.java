a97a9dfda8a309a2b6deff36ff4ee0cc40a1ec59
Thomas Heigl <thomas.heigl@gmail.com>
2020-08-09 10:35:03 +0200

    Upgrade copyright year to 2020 and add missing licence headers

--- src/com/esotericsoftware/kryo/util/DefaultClassResolver.java
+++ src/com/esotericsoftware/kryo/util/DefaultClassResolver.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2018, Nathan Sweet
+/* Copyright (c) 2008-2020, Nathan Sweet
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
