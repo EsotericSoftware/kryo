59d7532949e70bd65cc5d76c6d7ab33d42416ecc
Julia Boes <julia.boes@oracle.com>
2020-09-03 17:10:41 +0100

    add record serializer and test and update pom

--- src/com/esotericsoftware/kryo/serializers/RecordSerializer.java
+++ src/com/esotericsoftware/kryo/serializers/RecordSerializer.java
@@ -21,18 +21,196 @@
 package com.esotericsoftware.kryo.serializers;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.KryoException;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static com.esotericsoftware.minlog.Log.TRACE;
+import static com.esotericsoftware.minlog.Log.trace;
+import static java.lang.invoke.MethodType.methodType;
+
 public class RecordSerializer<T> extends ImmutableSerializer<T> {
+    private static final MethodHandle MH_IS_RECORD;
+    private static final MethodHandle MH_GET_RECORD_COMPONENTS;
+    private static final MethodHandle MH_GET_NAME;
+    private static final MethodHandle MH_GET_TYPE;
+    private static final MethodHandles.Lookup LOOKUP;
+
+    static {
+        MethodHandle MH_isRecord;
+        MethodHandle MH_getRecordComponents;
+        MethodHandle MH_getName;
+        MethodHandle MH_getType;
+        LOOKUP = MethodHandles.lookup();
+
+        try {
+            // reflective machinery required to access the record components
+            // without a static dependency on Java SE 14 APIs
+            Class<?> c = Class.forName("java.lang.reflect.RecordComponent");
+            MH_isRecord = LOOKUP.findVirtual(Class.class, "isRecord", methodType(boolean.class));
+            MH_getRecordComponents = LOOKUP.findVirtual(Class.class, "getRecordComponents",
+                    methodType(Array.newInstance(c, 0).getClass()))
+                    .asType(methodType(Object[].class, Class.class));
+            MH_getName = LOOKUP.findVirtual(c, "getName", methodType(String.class))
+                    .asType(methodType(String.class, Object.class));
+            MH_getType = LOOKUP.findVirtual(c, "getType", methodType(Class.class))
+                    .asType(methodType(Class.class, Object.class));
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            // pre-Java-14
+            MH_isRecord = null;
+            MH_getRecordComponents = null;
+            MH_getName = null;
+            MH_getType = null;
+        } catch (IllegalAccessException unexpected) {
+            throw new AssertionError(unexpected);
+        }
+
+        MH_IS_RECORD = MH_isRecord;
+        MH_GET_RECORD_COMPONENTS = MH_getRecordComponents;
+        MH_GET_NAME = MH_getName;
+        MH_GET_TYPE = MH_getType;
+    }
+
+    public RecordSerializer() {
+    }
 
     @Override
     public void write(Kryo kryo, Output output, T object) {
-        // do something
+        final Class<?> cls = object.getClass();
+        if (!isRecord(cls)) {
+            throw new KryoException(object + " is not a record");
+        }
+        for (RecordComponent rc : recordComponents(cls)) {
+            final Class<?> type = rc.type();
+            final String name = rc.name();
+            try {
+                if (TRACE) trace("kryo", "Write property: " + name + " (" + type.getName() + ")");
+                if (rc.type().isPrimitive()) {
+                    kryo.writeObject(output, componentValue(object, rc));
+                } else {
+                    kryo.writeObjectOrNull(output, componentValue(object, rc), type);
+                }
+            } catch (KryoException ex) {
+                ex.addTrace(name + " (" + type.getName() + ")");
+                throw ex;
+            } catch (Throwable t) {
+                KryoException ex = new KryoException(t);
+                ex.addTrace(name + " (" + type.getName() + ")");
+                throw ex;
+            }
+        }
     }
 
     @Override
     public T read(Kryo kryo, Input input, Class<? extends T> type) {
-        return null;
+        if (!isRecord(type)) {
+            throw new KryoException("Not a record (" + type + ")");
+        }
+        final RecordComponent[] recordComponents = recordComponents(type);
+        final Object[] values = new Object[recordComponents.length];
+        for (int i = 0; i < recordComponents.length; i++) {
+            final RecordComponent rc = recordComponents[i];
+            final String name = rc.name();
+            try {
+                if (TRACE) trace("kryo", "Read property: " + name + " (" + type.getName() + ")");
+                values[i] = rc.type().isPrimitive() ? kryo.readObject(input, rc.type())
+                        : kryo.readObjectOrNull(input, rc.type());
+            } catch (KryoException ex) {
+                ex.addTrace(name + " (" + type.getName() + ")");
+                throw ex;
+            } catch (Throwable t) {
+                KryoException ex = new KryoException(t);
+                ex.addTrace(name + " (" + type.getName() + ")");
+                throw ex;
+            }
+        }
+        return invokeCanonicalConstructor(type, recordComponents, values);
+    }
+
+    /** Returns true if, and only if, the given class is a record class. */
+    private boolean isRecord(Class<?> type) {
+        try {
+            return (boolean) MH_IS_RECORD.invokeExact(type);
+        } catch (Throwable t) {
+            throw new KryoException("Could not determine type (" + type + ")");
+        }
+    }
+
+    /** A record component, which has a name and a type. */
+    final static class RecordComponent {
+        private final String name;
+        private final Class<?> type;
+        RecordComponent(String name, Class<?> type) {
+            this.name = name;
+            this.type = type;
+        }
+        String name() { return name; }
+        Class<?> type() { return type; }
+    }
+
+    /**
+     * Returns an ordered array of the record components for the given record
+     * class. The order is that of the components in the record attribute of the
+     * class file.
+     */
+    private static <T> RecordComponent[] recordComponents(Class<T> type) {
+        try {
+            Object[] rawComponents = (Object[]) MH_GET_RECORD_COMPONENTS.invokeExact(type);
+            RecordComponent[] recordComponents = new RecordComponent[rawComponents.length];
+            for (int i = 0; i < rawComponents.length; i++) {
+                final Object comp = rawComponents[i];
+                recordComponents[i] = new RecordComponent(
+                        (String) MH_GET_NAME.invokeExact(comp),
+                        (Class<?>) MH_GET_TYPE.invokeExact(comp));
+            }
+            return recordComponents;
+        } catch (Throwable t) {
+            KryoException ex = new KryoException(t);
+            ex.addTrace("Could not retrieve record components (" + type.getName() + ")");
+            throw ex;
+        }
+    }
+
+    /** Retrieves the value of the record component for the given record object. */
+    private static Object componentValue(Object recordObject,
+                                         RecordComponent recordComponent) {
+        try {
+            MethodHandle MH_get = LOOKUP.findVirtual(recordObject.getClass(),
+                    recordComponent.name(),
+                    methodType(recordComponent.type()));
+            return (Object) MH_get.invoke(recordObject);
+        } catch (Throwable t) {
+            KryoException ex = new KryoException(t);
+            ex.addTrace("Could not retrieve record components ("
+                    + recordObject.getClass().getName() + ")");
+            throw ex;
+        }
+    }
+
+    /**
+     * Invokes the canonical constructor of a record class with the
+     * given argument values.
+     */
+    private static <T> T invokeCanonicalConstructor(Class<T> recordType,
+                                                RecordComponent[] recordComponents,
+                                                Object[] args) {
+        try {
+            Class<?>[] paramTypes = Arrays.stream(recordComponents)
+                    .map(RecordComponent::type)
+                    .toArray(Class<?>[]::new);
+            MethodHandle MH_canonicalConstructor =
+                    LOOKUP.findConstructor(recordType, methodType(void.class, paramTypes))
+                            .asType(methodType(Object.class, paramTypes));
+            return (T)MH_canonicalConstructor.invokeWithArguments(args);
+        } catch (Throwable t) {
+            KryoException ex = new KryoException(t);
+            ex.addTrace("Could not construct type (" + recordType.getName() + ")");
+            throw ex;
+        }
     }
 }
--- test/jdk14/RecordSerializerTest.java
+++ test/jdk14/RecordSerializerTest.java
@@ -20,8 +20,18 @@
 
 package jdk14;
 
+import com.esotericsoftware.kryo.KryoException;
 import com.esotericsoftware.kryo.KryoTestCase;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static java.lang.System.out;
+import static org.junit.Assert.assertEquals;
 
 public class RecordSerializerTest extends KryoTestCase {
     {
@@ -29,10 +39,277 @@ public class RecordSerializerTest extends KryoTestCase {
     }
 
     /** Test where the single object is a record. */
-    public record RecordRectangle (int height, int width, int x, int y) { }
+    public record RecordRectangle (String height, int width, long x, double y) { }
 
     @Test
     public void testBasicRecord() {
-        // test something
+        out.println("testBasicRecord\n");
+        kryo.register(RecordRectangle.class);
+
+        final var r1 = new RecordRectangle("one", 2, 3L, 4.0);
+        final var output = new Output(32);
+        kryo.writeObject(output, r1);
+        out.println("Serialized record: \n" + Arrays.toString(output.getBuffer()));
+
+        final var input = new Input(output.getBuffer(), 0, output.position());
+        final var r2 = kryo.readObject(input, RecordRectangle.class);
+        out.println("Deserialized record: \n" + r2);
+
+        doAssertEquals(r1, r2);
+        roundTrip(14, r1);
+        out.println("------\n");
+    }
+
+    /** Test where the single object is an empty record. */
+    public record EmptyRecord () { }
+
+    @Test
+    public void testEmptyRecord() {
+        out.println("testEmptyRecord\n");
+        kryo.register(EmptyRecord.class);
+
+        final var r1 = new EmptyRecord();
+        final var output = new Output(32);
+        kryo.writeObject(output, r1);
+        out.println("Serialized record: \n" + Arrays.toString(output.getBuffer()));
+
+        final var input = new Input(output.getBuffer(), 0, output.position());
+        final var r2 = kryo.readObject(input, EmptyRecord.class);
+        out.println("Deserialized record: \n" + r2);
+
+        doAssertEquals(r1, r2);
+        roundTrip(1, r1);
+        out.println("------\n");
+    }
+
+    /** Test deserialisation of an empty record where the input provides values.
+     *  In this case no values are read from the input.
+     */
+    @Test
+    public void testDeserializeEmptyRecordWithValues() {
+        out.println("testDeserializeEmptyRecordWithValues\n");
+        kryo.register(EmptyRecord.class);
+
+        final var input = new Input(new byte[]{1, 2, -128, 0});  // create bad input
+        out.println("Serialized record: \n" + Arrays.toString(input.getBuffer()));
+        final var r = kryo.readObject(input, EmptyRecord.class);
+        out.println("Deserialized record: \n" + r);
+        out.println("------\n");
+    }
+
+    /** Test deserialisation of a record where the number of input values exceeds
+     *  the number of record components. In this case values are read from the
+     *  input sequentially until the number of record components is met,
+     *  any additional input values are ignored.
+     */
+    public record RecordPoint (int x, int y) { }
+
+    @Test
+    public void testDeserializeWrongNumberOfValues() {
+        out.println("testDeserializeWrongNumberOfValues\n");
+        kryo.register(RecordPoint.class);
+
+        final var r1 = new RecordPoint(1,1);
+        final var input = new Input(new byte[]{2, 2, 2});  // create bad input
+        out.println("Serialized record: \n" + Arrays.toString(input.getBuffer()));
+        final var r2 = kryo.readObject(input, RecordPoint.class);
+        out.println("Deserialized record: \n" + r2);
+        doAssertEquals(r1, r2);
+        out.println("------\n");
+    }
+
+    /** Test where the record has an explicit constructor.*/
+    public record RecordWithConstructor (String height, int width, long x, double y) {
+        public RecordWithConstructor(String height) {
+            this(height, 20, 30L, 40.0);
+        }
+    }
+
+    @Test
+    public void testRecordWithConstructor() {
+        out.println("testRecordWithConstructor\n");
+        kryo.register(RecordWithConstructor.class);
+
+        final var r1 = new RecordWithConstructor("ten");
+        final var output = new Output(32);
+        kryo.writeObject(output, r1);
+        out.println("Serialized record: \n" + Arrays.toString(output.getBuffer()));
+
+        final var input = new Input(output.getBuffer(), 0, output.position());
+        final var r2 = kryo.readObject(input, RecordWithConstructor.class);
+        out.println("Deserialized record: \n" + r2);
+
+        doAssertEquals(r1, r2);
+        roundTrip(14, r1);
+        out.println("------\n");
+    }
+
+    /** Test where the record component object is a record. */
+    public record RecordOfRecord (RecordRectangle r) { }
+
+    @Test
+    public void testRecordOfRecord() {
+        out.println("testRecordOfRecord\n");
+        kryo.register(RecordOfRecord.class);
+        kryo.register(RecordRectangle.class);
+
+        final var r1 = new RecordOfRecord(new RecordRectangle("one", 2, 3L, 4.0));
+        final var output = new Output(32);
+        kryo.writeObject(output, r1);
+        out.println("Serialized record: \n" + Arrays.toString(output.getBuffer()));
+
+        final var input = new Input(output.getBuffer(), 0, output.position());
+        final var r2 = kryo.readObject(input, RecordOfRecord.class);
+        out.println("Deserialized record: \n" + r2);
+
+        doAssertEquals(r1, r2);
+        roundTrip(15, r1);
+        out.println("------\n");
+    }
+
+    /** Test where the single object is an array of records. */
+    @Test
+    public void testArrayOfRecords() {
+        out.println("testArrayOfRecords\n");
+        kryo.register(RecordPoint.class);
+        kryo.register(RecordPoint[].class);
+
+        final var arr = new RecordPoint[100];
+        IntStream.range(0, 100).forEach(i -> arr[i] = new RecordPoint(i, i+1));
+
+        roundTrip(375, arr);
+    }
+
+    /** Test where the record component object is an array of records. */
+    public record RecordWithArray (RecordRectangle[] recordArray) { }
+
+    @Test
+    public void testRecordWithArray() {
+        out.println("testRecordWithArray\n");
+        kryo.register(RecordWithArray.class);
+        kryo.register(RecordRectangle[].class);
+        kryo.register(RecordRectangle.class);
+
+        final var r1 = new RecordWithArray(new RecordRectangle[] {new RecordRectangle("one", 2, 3L, 4.0)});
+        final var output = new Output(32);
+        kryo.writeObject(output, r1);
+        out.println("Serialized record: \n" + Arrays.toString(output.getBuffer()));
+
+        final var input = new Input(output.getBuffer(), 0, output.position());
+        final var r2 = kryo.readObject(input, RecordWithArray.class);
+        out.println("Deserialized record: \n" + r2);
+
+        assertWithArrayEquals(r1, r2);
+        out.println("------\n");
+    }
+
+    private void assertWithArrayEquals(final RecordWithArray expected,
+                                         final RecordWithArray actual) {
+        assertEquals(expected.getClass(), actual.getClass());
+        final RecordRectangle[] expectedArray = expected.recordArray();
+        final RecordRectangle[] actualArray = actual.recordArray();
+        assertEquals(Array.getLength(expectedArray), Array.getLength(actualArray));
+        for (int i = 0; i < Array.getLength(expectedArray); i++) {
+            assertEquals(Array.get(expectedArray, i), Array.get(actualArray, i));
+        }
+    }
+
+    /** Test where record components are non-primitives with their default
+     *  value (null).
+     */
+    public record RecordWithNull (Object o, Number n, String s) { }
+
+    @Test
+    public void testRecordWithNull() {
+        out.println("testRecordWithNull\n");
+        kryo.register(RecordWithNull.class);
+        kryo.register(Object.class);
+        kryo.register(Number.class);
+        kryo.register(String.class);
+
+        final var r1 = new RecordWithNull(null, null, null);
+        final var output = new Output(32);
+        kryo.writeObject(output, r1);
+        out.println("Serialized record: \n" + Arrays.toString(output.getBuffer()));
+
+        final var input = new Input(output.getBuffer(), 0, output.position());
+        final var r2 = kryo.readObject(input, RecordWithNull.class);
+        out.println("Deserialized record: \n" + r2);
+
+        doAssertEquals(r1, r2);
+        roundTrip(4, r1);
+        out.println("------\n");
+    }
+
+    /** Test where record components are primitives with their default values. */
+    public record RecordWithDefaultValues(byte b, short s, int i, long l, float f, double d, char c, boolean bool) { }
+
+    @Test
+    public void testRecordWithPrimitiveDefaultValues() {
+        out.println("testRecordWithPrimitiveDefaultValues\n");
+        kryo.register(RecordWithDefaultValues.class);
+
+        final var r1 = new RecordWithDefaultValues(
+                (byte)0, (short)0, 0, 0l, 0.0f, 0.0d, '\u0000', false);
+        final var output = new Output(32);
+        kryo.writeObject(output, r1);
+        out.println("Serialized record: \n" + Arrays.toString(output.getBuffer()));
+
+        final var input = new Input(output.getBuffer(), 0, output.position());
+        final var r2 = kryo.readObject(input, RecordWithDefaultValues.class);
+        out.println("Deserialized record: \n" + r2);
+
+        doAssertEquals(r1, r2);
+        roundTrip(21, r1);
+        out.println("------\n");
+    }
+
+    /** Test where the an exception is thrown in the record constructor. */
+    public record PositivePoint (int x, int y) {
+        public PositivePoint { // compact syntax
+            if (x < 0)
+                throw new IllegalArgumentException("negative x:" + x);
+            if (y < 0)
+                throw new IllegalArgumentException("negative y:" + y);
+        }
+    }
+
+    @Test
+    public void testDeserializeRecordWithIllegalValue1() {
+        out.println("testDeserializeRecordWithIllegalValue1\n");
+        kryo.register(PositivePoint.class);
+
+        final var input = new Input(new byte[]{1, 2});  // create bad input of -1, 1
+        out.println("Serialized record: \n" + Arrays.toString(input.getBuffer()));
+        var e = expectThrows(IllegalArgumentException.class,
+                () -> kryo.readObject(input, PositivePoint.class));
+        assertEquals("negative x:-1", e.getMessage());
+        out.println("------\n");
+    }
+
+    @Test
+    public void testDeserializeRecordWithIllegalValue2() {
+        out.println("testDeserializeRecordWithIllegalValue2\n");
+        kryo.register(PositivePoint.class);
+
+        final var input = new Input(new byte[]{2, 1});  // create bad input of 1, -1
+        out.println("Serialized record: \n" + Arrays.toString(input.getBuffer()));
+        var e = expectThrows(IllegalArgumentException.class,
+                () -> kryo.readObject(input, PositivePoint.class));
+        assertEquals("negative y:-1", e.getMessage());
+        out.println("------\n");
+    }
+
+    static <T extends Throwable> T expectThrows(Class<T> throwableClass, Runnable task) {
+        try {
+            task.run();
+            throw new AssertionError("Exception not thrown");
+        } catch (KryoException ce) {
+            Throwable cause = ce.getCause();
+            if (!throwableClass.isInstance(cause)) {
+                throw new RuntimeException("expected: " + throwableClass + ", actual: " + cause);
+            }
+            return throwableClass.cast(cause);
+        }
     }
 }
