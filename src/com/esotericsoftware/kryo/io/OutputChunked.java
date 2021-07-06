ca876f6f776464fe55a1d66803e5f7df9230e8bc
jackjoesh <jackjoesh@sina.com>
2021-07-06 17:04:01 +0800

    avoid flush repeatedly when has finished flushing (#839)
    * avoid repeat flush when position finish
    
    * Formatting
    
    Co-authored-by: jerry.l <jerry.l@binance.com>
    Co-authored-by: Thomas Heigl <thomas.heigl@gmail.com>

--- src/com/esotericsoftware/kryo/io/OutputChunked.java
+++ src/com/esotericsoftware/kryo/io/OutputChunked.java
@@ -58,8 +58,9 @@ public class OutputChunked extends Output {
 			} catch (IOException ex) {
 				throw new KryoException(ex);
 			}
+		} else {
+			super.flush();
 		}
-		super.flush();
 	}
 
 	private void writeChunkSize () throws IOException {
@@ -93,7 +94,7 @@ public class OutputChunked extends Output {
 		outputStream.write(size);
 	}
 
-	/** Marks the curent written data as the end of a chunk. This chunk can then be skipped when reading. */
+	/** Marks the current written data as the end of a chunk. This chunk can then be skipped when reading. */
 	public void endChunk () {
 		flush();
 		if (TRACE) trace("kryo", "End chunk.");
