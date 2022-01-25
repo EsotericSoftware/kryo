92416635ad5b2620725c34cd457ded7727928183
Thomas Heigl <thomas.heigl@gmail.com>
2022-01-25 13:16:23 +0100

    Use IdentityMap instead of CuckooObjectMap in class resolver (#877)
    * Use IdentityMap instead of CuckooObjectMap in class resolver

--- src/com/esotericsoftware/kryo/util/DefaultClassResolver.java
+++ src/com/esotericsoftware/kryo/util/DefaultClassResolver.java
@@ -37,7 +37,7 @@ public class DefaultClassResolver implements ClassResolver {
 	protected Kryo kryo;
 
 	protected final IntMap<Registration> idToRegistration = new IntMap<>();
-	protected final CuckooObjectMap<Class, Registration> classToRegistration = new CuckooObjectMap<>();
+	protected final IdentityMap<Class, Registration> classToRegistration = new IdentityMap<>();
 
 	protected IdentityObjectIntMap<Class> classToNameId;
 	protected IntMap<Class> nameIdToClass;
