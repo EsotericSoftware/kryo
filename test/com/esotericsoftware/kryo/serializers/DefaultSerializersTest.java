53d4682b5b06a91eeb05e8cfd6ffb87156fdb62a
Stuart Sharpe <a7832@drazi.com>
2022-03-19 17:46:32 +0000

    #889 Serializer for java.sql.Timestamp which preserves nanoseconds

--- test/com/esotericsoftware/kryo/serializers/DefaultSerializersTest.java
+++ test/com/esotericsoftware/kryo/serializers/DefaultSerializersTest.java
@@ -225,6 +225,28 @@ class DefaultSerializersTest extends KryoTestCase {
 	}
 
 	@Test
+	void testTimestampSerializer () {
+		kryo.addDefaultSerializer(java.sql.Timestamp.class, DefaultSerializers.TimestampSerializer.class);
+		kryo.register(java.sql.Timestamp.class);
+		roundTrip(11, newTimestamp(Long.MIN_VALUE+808, 0)); // Smallest valid size
+		roundTrip(15, newTimestamp(Long.MIN_VALUE+808, 999_999_999));
+		roundTrip(11, newTimestamp(Long.MAX_VALUE, 0));
+		roundTrip(14, newTimestamp(Long.MAX_VALUE, 268_435_455)); // Largest valid size
+		roundTrip(3, newTimestamp(0, 0));
+		roundTrip(7, newTimestamp(0, 999_999_999));
+		roundTrip(8, newTimestamp(1234567, 123_456_789));
+		roundTrip(11, newTimestamp(-1234567, 0));
+		roundTrip(11, newTimestamp(-1234567, 1));
+		roundTrip(14, newTimestamp(-1234567, 123_456_789));
+	}
+	
+	private java.sql.Timestamp newTimestamp(long time, int nanos) {
+		java.sql.Timestamp t = new java.sql.Timestamp(time);
+		t.setNanos(nanos);
+		return t;
+	}
+
+	@Test
 	void testBigDecimalSerializer () {
 		kryo.register(BigDecimal.class);
 		kryo.register(BigDecimalSubclass.class);
