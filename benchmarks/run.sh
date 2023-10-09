c2b9e384291fdeb40d55d97a5e723048292c826d
Nathan Sweet <nathan.sweet@gmail.com>
2023-10-09 18:45:14 -0400

    Fixed Eclipse benchmark generation from annotations and benchmark run.sh paths.

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 args="-f 4 -wi 5 -i 3 -t 2 -w 2s -r 2s -rf csv -rff"
-jmh="$JAVA_HOME/bin/java -cp ../bin;../lib/*;lib/* com.esotericsoftware.kryo.benchmarks.KryoBenchmarks $args"
+jmh="$JAVA_HOME/bin/java -cp ../eclipse/bin;../eclipse/.apt_generated;../lib/*;lib/* com.esotericsoftware.kryo.benchmarks.KryoBenchmarks $args"
 
 set -ex
 
+mkdir -p charts/results
 $jmh charts/results/fieldSerializer.csv FieldSerializerBenchmark
 $jmh charts/results/array.csv ArrayBenchmark
 $jmh charts/results/string.csv StringBenchmark
