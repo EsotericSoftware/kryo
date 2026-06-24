055477133e2e6731b43367965b167508014b5050
Thomas Heigl <thomas.heigl@gmail.com>
2026-06-24 06:18:53 +0200

    Revert "#829 Wrap allocation errors in `KryoException` when reading and writing fields"
    This reverts commit 1d72a60844fdd75250f09f74fd493485092026fe.

--- src/com/esotericsoftware/kryo/serializers/FieldSerializer.java
+++ src/com/esotericsoftware/kryo/serializers/FieldSerializer.java
@@ -110,7 +110,7 @@ public class FieldSerializer<T> extends Serializer<T> {
 				fields[i].write(output, object);
 			} catch (KryoException e) {
 				throw e;
-			} catch (OutOfMemoryError | Exception e) {
+			} catch (Exception e) {
 				throw new KryoException("Error writing " + fields[i] + " at position " + output.position(), e);
 			}
 		}
@@ -130,8 +130,8 @@ public class FieldSerializer<T> extends Serializer<T> {
 			try {
 				fields[i].read(input, object);
 			} catch (KryoException e) {
-				throw e;
-			} catch (OutOfMemoryError | Exception e) {
+				throw e;				
+			} catch (Exception e) {
 				throw new KryoException("Error reading " + fields[i] + " at position " + input.position(), e);
 			}
 		}
